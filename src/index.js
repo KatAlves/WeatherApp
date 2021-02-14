83b9b2f7d8eec3d26d584c7c252a180aa6a1e640
KatAlves <77025810+KatAlves@users.noreply.github.com>
2021-02-14 00:03:26 +0000

    JS in progress
    //Date / Time, //Units //Show WeatherCondition //GPS Button

---
+++ src/index.js
@@ -0,0 +1,104 @@
+//Date / Time
+
+function formatDate(Date) {
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = weekDays[currentDate.getDay()];
+  let showDay = document.querySelector("#date");
+  showDay.innerHTML = day;
+
+  let hours = currentDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let showTime = document.querySelector("#time");
+  showTime.innerHTML = `${hours}:${minutes}`;
+}
+
+let currentDate = new Date();
+
+formatDate(currentDate);
+
+//Units
+
+function showCelsius() {
+  let temperature = document.querySelector("#temp-special");
+  temperature.innerHTML = 5;
+}
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", showCelsius);
+
+function showFahrenheit() {
+  let temperature = document.querySelector("#temp-special");
+  temperature.innerHTML = 50;
+}
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", showFahrenheit);
+
+//Show WeatherCondition
+
+function displayWeatherCondition(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temp-special");
+  temperatureElement.innerHTML = `${temperature}º`;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+//Show City
+
+function searchCity(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+//GPS Button
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "8928143376b6d241d15f969b0a4f9c51";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let gpsButton = document.querySelector("#gpsButton");
+gpsButton.addEventListener("click", getPosition);
+
+searchCity("Lisbon");
